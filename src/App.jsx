bcfb0d2ed0b421407968dec18aeaf6b47fff3c61
orshah <orshpov@gmail.com>
2023-08-11 19:33:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import PostsList from "./features/posts/PostsList";
+
+function App() {
+  return (
+    <main className="App">
+      <PostsList />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/features/posts/PostsList.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { selectAllPosts } from "./postsSlice";
+
+import React from "react";
+
+const PostsList = () => {
+  const posts = useSelector(selectAllPosts);
+
+  const renderedPosts = posts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+    </article>
+  ));
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
+
+export default PostsList;
