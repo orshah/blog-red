00aa112eda9271f7adef78eae0b8844b94f736a9
orshah <orshpov@gmail.com>
2023-08-11 23:42:04 -0500

    done

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -2,17 +2,25 @@ import { useSelector } from "react-redux/es/hooks/useSelector";
 import { selectAllPosts } from "./postsSlice";
 import React from "react";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderedPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
+      <ReactionButtons post={post} />
     </article>
   ));
   return (
